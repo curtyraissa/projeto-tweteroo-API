898f2497e7d73f30967a35ca5578ce14642e4681
curtyraissa <raissacurty@gmail.com>
2023-04-08 12:49:27 -0300

    criado a porta 5000 para utilizar no servidor

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import express from 'express'; // Importa o express da biblioteca
 
 const app = express(); // Cria uma instância do servidor
+
+
+app.listen(5000, () => console.log("Running server on port 5000"));
