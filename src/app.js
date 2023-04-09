81a8201609c37c00b32a13343f2f19f371df021e
curtyraissa <raissacurty@gmail.com>
2023-04-09 02:14:54 -0300

    concluido post /tweets

--- src/app.js
+++ src/app.js
@@ -7,20 +7,9 @@ const app = express(); //criacao do app servidor
 app.use(cors()); // acessar as portas
 app.use(express.json()); //ler no formato json
 
-const tweetsCompleto = [
-	{
-		username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-	}
-]
-app.get("/tweets", (request, response) => {
-  // const { username } = request.params  //const username = request.params.username
-  // const tweet = tweetsCompleto.find((item) => item.username === username)
-  response.send(tweetsCompleto)
-})
-
 const usuarios = []
+const tweets = []
+
 app.post("/sign-up", (request, response) => {
   const { username, avatar } = request.body
   const novoUsuario = { username, avatar }
@@ -28,13 +17,26 @@ app.post("/sign-up", (request, response) => {
   response.send("OK")
 })
 
-const tweets = []
 app.post("/tweets", (request, response) => {
   const { username, tweet } = request.body
   const novoTweet = { username, tweet }
+  const cadastrado = usuarios.find(item => item.username === username)
+  if(cadastrado){
   tweets.push(novoTweet);
   response.send("OK");
+  } else { response.send("UNAUTHORIZED")}
 })
 
+app.get("/tweets/:username", (request, response) => {
+  const { username } = request.params
+  const tweetsCompleto = tweets.find((item) => {
+    if(item.username === username){
+      return item.avatar
+    }
+  })
+  response.status(200).send(tweetsCompleto)
+})
+
+//rodando servidor na porta 5000
 const PORT = 5000;
 app.listen(PORT, () => { console.log(`servidor rodando na porta ${PORT}`) });
